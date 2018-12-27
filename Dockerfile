c9845d09c3a902663ea7dcaf770569824c813b25
Andry Ranaivo <a.ranaivo@treeptik.fr>
2018-12-27 11:37:16 +0000

    Dockerfile, index

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM php:5.6-apache
 COPY public /var/www/html/
 #ENV ENVNAME=dev LOGIN=ligindev PWD=pwddev
+EXPOSE 80
--- public/index.php
+++ public/index.php
@@ -6,9 +6,9 @@
   </head>
   <body>
 	<?php
-		echo "LOGIN IN"; echo getenv('ENVNAME'); echo "namespace: "; echo getenv("LOGIN");
-		echo "PASSWORD IN"; echo getenv('ENVNAME'); echo "namespace: "; echo getenv("PWD");
-		echo getenv('path');
+		echo "LOGIN IN"; echo getenv('ENVNAME'); echo "namespace is: "; echo getenv("LOGIN"); echo "\r\n";
+		echo "PASSWORD IN"; echo getenv('ENVNAME'); echo "namespace is: "; echo getenv("PWD"); echo "\r\n";
+		echo "PATH VARIABLE"; echo getenv('path'); echo "\r\n";
 	?>
   </body>
 </html>
