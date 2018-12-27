3c8edf5fadc0032e7268480c42d7e305f7247858
Andry Ranaivo <a.ranaivo@treeptik.fr>
2018-12-27 12:46:40 +0000

    login pwd

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
   </head>
   <body>
 	<?php
-		echo "LOGIN IN"; echo getenv('ENVNAME'); echo "namespace is: "; echo getenv("APPLOGIN"); echo "\r\n";
-		echo "PASSWORD IN"; echo getenv('ENVNAME'); echo "namespace is: "; echo getenv("APPPWD"); echo "\r\n";
+		echo "LOGIN IN"; echo getenv('ENVNAME'); echo " namespace is: "; echo getenv('APPLOGIN'); echo "<br>\r\n";
+		echo "PASSWORD IN"; echo getenv('ENVNAME'); echo " namespace is: "; echo getenv('APPPWD'); echo "<br>\r\n";
 		echo "PATH VARIABLE"; echo getenv('path'); echo "\r\n";
 	?>
   </body>
