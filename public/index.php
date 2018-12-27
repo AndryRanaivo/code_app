a2f53d209b3a16dfddac22a4bfd393dfae61b799
Andry Ranaivo <a.ranaivo@treeptik.fr>
2018-12-27 12:58:00 +0000

    index

--- public/index.php
+++ public/index.php
@@ -2,13 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8"
-    <title>Docker PHP Hello World</title>
+    <title>PRINT ENV VARS </title>
   </head>
   <body>
 	<?php
-		echo "LOGIN IN"; echo getenv('ENVNAME'); echo " namespace is: "; echo getenv('APPLOGIN'); echo "<br>\r\n";
-		echo "PASSWORD IN"; echo getenv('ENVNAME'); echo " namespace is: "; echo getenv('APPPWD'); echo "<br>\r\n";
-		echo "PATH VARIABLE"; echo getenv('path'); echo "\r\n";
+		echo "<br> LOGIN in "; echo getenv('ENVNAME'); echo " namespace is: "; echo getenv('APPLOGIN'); echo "<br>\r\n";
+		echo "<br> PASSWORD in "; echo getenv('ENVNAME'); echo " namespace is: "; echo getenv('APPPWD'); echo "<br>\r\n";
+		echo "<br>PATH VARIABLE: "; echo getenv('path'); echo "\r\n";
 	?>
   </body>
 </html>
